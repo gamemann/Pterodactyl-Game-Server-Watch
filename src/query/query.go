e2ea15543f333146b6af4092430abae819566a6a
Christian Deacon <gamemann@gflclan.com>
2020-05-09 18:17:42 +0000

    Added A2S_INFO/query package.

---
+++ src/query/query.go
@@ -0,0 +1,56 @@
+package query
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+// The A2S_INFO request.
+var query = []byte{0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00}
+
+// Creates a UDP connection using the host and port.
+func CreateConnection(host string, port int) (*net.UDPConn, error) {
+	var UDPC *net.UDPConn
+
+	// Combine host and port.
+	fullHost := host + ":" + string(port)
+
+	// Resolve host name if any. Otherwise, use IP.
+	ip, err := net.ResolveUDPAddr("udp4", fullHost)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return UDPC, errors.New("NoResolve")
+	}
+
+	// Attempt to open a UDP connection.
+	UDPC, err = net.DialUDP("udp4", nil, ip)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return UDPC, errors.New("NoConnection")
+	}
+
+	return UDPC, nil
+}
+
+// Sends an A2S_INFO request to the host and port specified in the arguments.
+func SendRequest(conn *net.UDPConn) {
+	conn.Write(query)
+}
+
+// Checks for A2S_INFO response. Returns true if it receives a response. Returns false otherwise.
+func CheckResponse(conn *net.UDPConn) bool {
+	buffer := make([]byte, 256)
+
+	_, _, err := conn.ReadFromUDP(buffer)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
