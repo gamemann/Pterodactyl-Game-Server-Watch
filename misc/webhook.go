f6aa5f5c6fdc088b072cb350365924361900a5c6
Christian Deacon <gamemann@gflclan.com>
2021-06-22 09:09:25 -0500

    Add Slack Web Hook support and revamp options.

--- misc/webhook.go
+++ misc/webhook.go
@@ -15,7 +15,11 @@ type DiscordWH struct {
 	AllowMentions bool   `json:"allowed_mentions"`
 }
 
-func DiscordWebHook(id string, token string, contents string, username string, avatarurl string, allowmentions bool) bool {
+type SlackWH struct {
+	Text string `json:"text"`
+}
+
+func DiscordWebHook(url string, contents string, username string, avatarurl string, allowmentions bool) bool {
 	var data DiscordWH
 
 	// Build out JSON/form data for Discord web hook.
@@ -33,12 +37,45 @@ func DiscordWebHook(id string, token string, contents string, username string, a
 		return false
 	}
 
-	// Build endpoint.
-	urlstr := "https://discordapp.com/api/webhooks/" + id + "/" + token
+	// Setup HTTP POST request with form data.
+	client := &http.Client{Timeout: time.Second * 5}
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(datajson))
+
+	// Set content type to JSON.
+	req.Header.Set("Content-Type", "application/json")
+
+	// Perform HTTP request and check for errors.
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return false
+	}
+
+	resp.Body.Close()
+
+	return true
+}
+
+func SlackWebHook(url string, contents string) bool {
+	var data SlackWH
+
+	// Build out JSON/form data for Slack web hook.
+	data.Text = contents
+
+	// Convert interface to JSON data string.
+	datajson, err := json.Marshal(data)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return false
+	}
 
 	// Setup HTTP POST request with form data.
 	client := &http.Client{Timeout: time.Second * 5}
-	req, _ := http.NewRequest("POST", urlstr, bytes.NewBuffer(datajson))
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(datajson))
 
 	// Set content type to JSON.
 	req.Header.Set("Content-Type", "application/json")
